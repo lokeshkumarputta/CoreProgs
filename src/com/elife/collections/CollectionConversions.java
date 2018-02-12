8db89b4ab91d39cd4b369aabb1d04f3c258a2639
Lokeshkumar <lokeshkumarputta@gmail.com>
2018-02-12 17:02:45 +0530

    Revert "Revert "added""
    This reverts commit 65260ca407d3fb6dbdfda476fcf52ad1a8d0b043.

--- src/com/elife/collections/CollectionConversions.java
+++ src/com/elife/collections/CollectionConversions.java
@@ -1,3 +1,4 @@
+package com.elife.collections;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
---
+++ src/com/elife/collections/CollectionPrograms.java
@@ -0,0 +1,44 @@
+package com.elife.collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CollectionPrograms {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Map<Integer, String> map = new HashMap<>();
+		
+		 map.put(4,"Mango");
+		    map.put(3,"Apple");
+		    map.put(5,"Orange");
+		    map.put(8,"Fruits");
+		    map.put(23,"Vegetables");
+		    map.put(1,"Zebra");
+		    map.put(5,"Yellow");
+		
+		// Normal HashMap Iteration
+		for(Map.Entry<Integer, String> entry : map.entrySet()) {
+			System.out.println("Keys " + entry.getKey() + "\t" + " values " +entry.getValue());
+		}
+		System.out.println("******************************");
+		
+		
+		TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>();
+		for (Map.Entry entry : map.entrySet()) {
+			treeMap.put((String) entry.getValue(),(Integer)entry.getKey());
+		    
+		}
+		System.out.println(treeMap);
+		
+		System.out.println("******************************");
+		// Sorting on Values By using Java 8
+		map.entrySet().stream().sorted(
+				(k1 , k2) -> k1.getValue().compareTo(k2.getValue())).forEach(k -> System.out.println(k.getKey() + " : " + k.getValue()));
+		
+		System.out.println("******************************");
+		
+	}
+
+}
