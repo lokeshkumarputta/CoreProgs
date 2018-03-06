57ab57eac2e9d20690480e42a1d16c1a09d5619f
Lokeshkumar <lokeshkumarputta@gmail.com>
2018-03-06 11:22:57 +0530

    testing from eclipse neon
    Signed-off-by: Lokeshkumar <lokeshkumarputta@gmail.com>

--- src/com/elife/corejavaprogs/StringIteration.java
+++ src/com/elife/corejavaprogs/StringIteration.java
@@ -3,7 +3,6 @@ package com.elife.corejavaprogs;
 public class StringIteration {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		String str1="abcadbcg";
 		String str2="bc";
